8a44f9688df1e6cd0215547eefceb9dc2607ab4d
giovannimazzi <gioelor@libero.it>
2025-12-18 15:54:26 +0100

    01-arrays-3

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -11,7 +11,8 @@ teachers[4] = "Patrick";
 console.log("2) ", teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers.pop();
+console.log(`3) lastTeacher: ${lastTeacher}`, teachers);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = null;
