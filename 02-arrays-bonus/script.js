6e038437dc86b0db0290435cf948663bfbf14fe8
giovannimazzi <gioelor@libero.it>
2025-12-18 16:10:18 +0100

    02-arrays-bonus-1

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -1,16 +1,14 @@
-const teachers = [
-  'Nathan',
-  'Ed',
-  'Fabio',
-  'Phil',
-  'Carlo',
-  'Lewis',
-  'Luca'
-]; // NON MODIFICARE QUESTA VARIABILE
+const teachers = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"]; // NON MODIFICARE QUESTA VARIABILE
+console.table(teachers);
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+let temp = [];
+for (let i = 0; i < teachers.length; i++) {
+  temp.unshift(teachers[i]);
+}
+const reversedTeachers = temp;
+console.log(`1) `, reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
