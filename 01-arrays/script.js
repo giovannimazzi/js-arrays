392d1e61f21938605180c86e6e80ea47070aa9ee
giovannimazzi <gioelor@libero.it>
2025-12-18 15:52:18 +0100

    01-arrays-2

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -1,11 +1,14 @@
 const teachers = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"]; // NON MODIFICARE QUESTA VARIABILE
+console.table(teachers);
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
 const fourthTeacher = teachers[3];
-console.log(`fourthTeacher: ${fourthTeacher}`);
+console.log(`1) fourthTeacher: ${fourthTeacher}`);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+teachers[4] = "Patrick";
+console.log("2) ", teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = null;
